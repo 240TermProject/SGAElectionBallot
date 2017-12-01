0f3f062d62103af388edea8b00e9dd1801838fae
wynton <kirkp1wc@cmich.edu>
2017-11-30 19:06:13 -0500

    Formated the printing

--- ballot/models.py
+++ ballot/models.py
@@ -4,9 +4,12 @@ from django.utils import timezone
 class Candidate(models.Model):
     name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return "%s" % self.name
+
 class Vote(models.Model):
     rank = models.IntegerField()
-    candidate = models.ForeignKey(Candidate)
+    candidate = models.ForeignKey(Candidate, null=True)
 
 class Voter(models.Model):
-    globalID = models.CharField (max_length=200)
+    globalID = models.CharField (max_length=200, null=False)
--- ballot/views.py
+++ ballot/views.py
@@ -6,6 +6,8 @@ from .models import Vote, Candidate
 from django.shortcuts import render, get_object_or_404
 from .forms import VoteForm
 from django.http import HttpResponseRedirect
+import operator
+from random import randint
 
 def vote_detail(request):
     if request.method == "GET":
@@ -15,7 +17,9 @@ def vote_detail(request):
             context["candidates"].append(candidate)
         return render(request, "ballot/vote_detail.html", context)
 
-
+def validate_globalID(request):
+    pass
+    #stuff
 
 def cast_votes(request):
     if request.method == "GET":
@@ -26,6 +30,8 @@ def cast_votes(request):
         return render(request, "ballot/cast_votes.html", context)
     elif request.method == "POST":
         post = request.POST
+        vote_list = []
+        voter_id = ("%d%d%d%d%d" %(randint(0, 9), randint(0, 9), randint(0, 9), randint(0, 9), randint(0, 9)))
         for rank_key, candidate_pk in post.items():
             if rank_key[0:4] == "rank":
                 final_rank = int(rank_key[5])
@@ -33,4 +39,13 @@ def cast_votes(request):
                 new_vote.rank = final_rank
                 new_vote.candidate = Candidate.objects.get(pk=int(candidate_pk))
                 new_vote.save()
+                vote_list.append([new_vote.rank, new_vote.candidate])
+        vote_list.sort(key=lambda tup: tup[0])
+        vote_list.insert(0, voter_id)
+        with open('voter_data.txt', 'a') as voter_data:
+            voter_data.write("%s: " % (vote_list[0]))
+            for thing in vote_list[1:]:
+                voter_data.write("%s, " % (thing))
+            voter_data.write("\n")
+
         return HttpResponseRedirect(reverse("detail"))
